024405549b00ac6aa5ecca944775261f29406e94
Tuan Tran <langthamht047@gmail.com>
2022-11-16 05:38:12 -0800

    connected mongodb

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,12 @@ const dotenv =require('dotenv')
 const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose')
 
-
+dotenv.config()
 const app = express();
 
+mongoose.connect(process.env.MONGODB_URL,() =>{
+    console.log("CONNECTED TO MONGODB")
+})
 app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
